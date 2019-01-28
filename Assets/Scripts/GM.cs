cfe832a348402537c1907e6a27c0dd50f03559a2
rahuldshetty <35rahuldshetty@gmail.com>
2019-01-29 00:59:17 +0530

    new update

---
+++ Assets/Scripts/GM.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GM : MonoBehaviour
+{
+    public GameObject food;
+
+    public Text Play ;
+
+
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+    public GameObject menuScreen;
+
+    private bool isGameStarted = false;
+
+    public float timeDuration = 5.0f;
+    private float timeInterval = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void gamePlay()
+    {
+        if (!isGameStarted)
+        {
+            isGameStarted = true;
+            Time.timeScale = 1;
+            menuScreen.SetActive(false);
+        }
+        else
+        {
+            SceneManager.LoadScene("game");
+        }
+
+    }
+
+
+    public void gameQuit()
+    {
+        Application.Quit();
+    }
+
+    public void gameOver()
+    {
+        Play.text = "Menu";
+        Time.timeScale = 0f;
+        menuScreen.SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timeInterval>0)
+        {
+            timeInterval -= Time.deltaTime;
+        }
+        else
+        {
+            Spawn();
+            timeInterval = timeDuration;
+        }
+    }
+
+    void Spawn()
+    {
+        int x = (int)(Random.Range(borderLeft.position.x+5,borderRight.position.x-5));
+        int y = (int)(Random.Range(borderBottom.position.y+5, borderTop.position.y-5));
+        Instantiate(food,new Vector2(x,y) , Quaternion.identity);
+    }
+
+}
---
+++ Assets/Scripts/Snake.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+
+
+public class Snake : MonoBehaviour
+{
+    private float speed = 0.20f;
+    Vector2 vector = Vector2.up;
+    Vector2 moveVector;
+    public Text scoreText;
+   
+    bool ate = false;
+
+    public GameObject tailPrefab;
+
+    private float timeToCall = 0.3f;
+
+    
+
+    private int scoreCount;
+    private List<Transform> tail = new List<Transform>();
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+        scoreCount = 0;
+        InvokeRepeating("Movement", timeToCall, speed);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            vector = Vector2.right;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            vector = Vector2.left;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            vector = Vector2.up;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            vector = Vector2.down;
+        }
+        moveVector = vector /3f;
+    }
+
+    void Movement()
+    {
+        Vector2 oldHeadPos = transform.position; 
+
+        transform.Translate(moveVector);
+
+        if (ate)
+        {
+            GameObject g = (GameObject)Instantiate(tailPrefab, oldHeadPos, Quaternion.identity);
+            scoreCount++;
+            scoreText.text = scoreCount + "";
+            tail.Insert(0, g.transform);
+            ate = false;
+            timeToCall -= 0.002f;
+            FindObjectOfType<GM>().timeDuration -= 0.0005f;
+        }
+
+        else if(tail.Count>0)
+        {
+            tail.Last().position = oldHeadPos;
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name.StartsWith("Food"))
+        {
+            ate = true;
+            Destroy(collision.gameObject);
+
+        }
+        else
+        {
+            Debug.Log(collision.name);
+            if (collision.gameObject.transform!=tail[0] || collision.name.StartsWith("Border"))
+            {
+
+                FindObjectOfType<GM>().gameOver();
+            }
+
+        }
+    }
+
+
+}
